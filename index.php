3c6b8f554acf180459cfcc9e7a0345a0c5809e38
torreshannai <torresorozcocesiahannai@gmail.com>
2024-07-17 02:48:20 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>CYBERNET</title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/aos.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-gymso-style.css">
+
+</head>
+<body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+
+    <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+
+            <a class="navbar-brand" href="indexx.php">CYBERNET</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a href="#INICIO" class="nav-link smoothScroll">INICIO</a>
+                    </li>
+
+                    <li class="nav-item">
+                    <a href="encuesta.php" class="nav-item nav-link">ENCUESTA</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="contactanos.php" class="nav-link smoothScroll">CONTACTANOS</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="pag inte 2.php" class="nav-link smoothScroll">ALCANCE</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="infracom.php" class="nav-link smoothScroll">INFRACOM</a>
+                    </li>
+                </ul>
+
+            
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+
+     <!-- HERO -->
+     <section class="hero d-flex flex-column justify-content-center align-items-center" id="INICIO">
+
+            <div class="bg-overlay"></div>
+
+               <div class="container">
+                    <div class="row">
+
+                         <div class="col-lg-8 col-md-10 mx-auto col-12">
+                              <div class="hero-text mt-5 text-center">
+
+                                    <h6 data-aos="fade-up" data-aos-delay="300">PROYECTOS DE INFRAESTRUCTURA DE COMUNICACIONES</h6>
+
+                    
+                                    <h1 class="display-2 font-family: 'Montserrat'; text-white mb-4" data-aos="fade-up" data-aos-delay="500">CYBERNET</h1>
+                                    <a href="#feature" class="btn custom-btn mt-3" data-aos="fade-up" data-aos-delay="600"></a>
+
+                                    <a class="btn btn-light font-weight-bold py-3 px-5 mt-2 btn-scroll" href="#QUIENES">CONOCENOS</a>
+                                   
+                              </div>
+                         </div>
+
+                    </div>
+               </div>
+     </section>
+
+
+   <!-- FEATURE -->
+   <section class="feature" id="feature">
+        <div class="container">
+            <div class="row">
+
+                <div class="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
+                    <h2 class="mb-3 text-white" data-aos="fade-up">¿CUALES SON NUESTROS OBJETIVOS?</h2>
+
+                    <h6 class="mb-4 text-white" data-aos="fade-up">Objetivos específicos</h6>
+
+                    <p data-aos="fade-up" data-aos-delay="200">-Instalar pantallas de entretenimiento en áreas de cardio y entrenamiento de fuerza dentro del gimnasio, que ofrezcan a los clientes acceso a una variedad de contenido multimedia, incluyendo programas de televisión, videos motivacionales y consejos de ejercicio, con el fin de mejorar la experiencia del usuario y mantenerlos comprometidos durante sus sesiones de entrenamiento.</p>
+</br>
+                    <p data-aos="fade-up" data-aos-delay="200"> -Integrar tecnologías de transmisión en vivo en las pantallas de entretenimiento, permitiendo a los clientes acceder a clases en vivo, eventos deportivos y programas especiales desde la comodidad del gimnasio, brindando así una experiencia interactiva y enriquecida que fomente la participación y la lealtad de los clientes. </p>
+
+                    <a href="#" class="btn custom-btn bg-color mt-3" data-aos="fade-up" data-aos-delay="300" data-toggle="modal" data-target="#membershipForm">¡Únete a nosotros en Pato Fit! </a>
+                </div>
+
+                <div class="mr-lg-auto mt-3 col-lg-4 col-md-6 col-12">
+                     <div class="about-working-hours">
+                          <div>
+
+                                <h2 class="mb-4 text-white" data-aos="fade-up" data-aos-delay="500">OBJETIVO GENERAL</h2>
+
+                            
+
+                                <p data-aos="fade-up" data-aos-delay="800">Diseñar y proporcionar servicios de comunicaciones para los clientes del gimnasio.</p>
+
+                               
+                               </div>
+                          </div>
+                     </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+     <!-- RUTINAS -->
+     <section class="about section" id="RUTINAS">
+        <div class="container">
+             <div class="row">
+
+     
+    
+      
+ 
+
+                            <div class="mt-lg-5 mb-lg-0 mb-4 col-lg-5 col-md-10 mx-auto col-12">
+                                <h2 class="mb-4" data-aos="fade-up" data-aos-delay="300" id="QUIENES">¿QUIENES SOMOS?</h2>
+
+                                <p data-aos="fade-up" data-aos-delay="400">
+                                Somos alumnos de tercer cuatrimestre en la carrera de Tecnologías de la Información Área Redes Digitales y se realizara un proyecto integrador, por parte de la división de Tecnologías de la Información y Comunicación. Estos atributos, enfocados en la calidad, pertinencia, intensidad, continuidad, polivalencia y flexibilidad, sirven como pilares para la formación de profesionales versátiles y adaptados a las demandas del entorno. “Integradora” se presenta como un proyecto integrador en el sentido más profundo de la palabra. Va más allá de la aplicación de conocimientos teóricos convencionales, representando la fusión y cohesión de disciplinas diversas, estructuradas en tres capítulos distintos.</p>
+
+                            
+
+                            </div>
+
+                            <div class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="700">
+                                <div class="team-thumb">
+                                    <img src="images/team/CAMARAS.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                        <h3>INNOVACION</h3>
+                                        <span></span>
+
+                                      
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mr-lg-auto mt-5 mt-lg-0 mt-md-0 col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="800">
+                                <div class="team-thumb">
+                                    <img src="images/team/RAMON.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                        <h3>COMPROMISO</h3>
+                                       
+                                           
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+               </div>
+     </section>
+
+
+    
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/aos.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
+</html>
